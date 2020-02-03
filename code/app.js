4e0206681757904debc63d3d1abf9215910aa561
Unsam98 <samorekoya.17@gmail.com>
2020-02-03 11:11:13 +0000

    Reg page UI, need to add functionality

---
+++ code/app.js
@@ -0,0 +1,4 @@
+const username = document.querySelector('#username');
+const password = document.querySelector('#password');
+const email = document.querySelector('#email');
+const coachMode = document.querySelector('#coachMode');
